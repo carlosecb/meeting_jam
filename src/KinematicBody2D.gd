88ac2f838395113c9825333a931b9ec781a68e8a
Guido Neulaender <gneulaender@localhost.localdomain>
2020-05-19 23:55:17 -0300

    Improved moviment

--- src/KinematicBody2D.gd
+++ src/KinematicBody2D.gd
@@ -14,14 +14,17 @@ func _ready():
 
 const gravity = 10
 export (int) var speed = 100
-export (int) var jump_power = -350
+export (int) var jump_power = -250
+export (int) var jump_extra = -5
 var on_glide = false
 export (int) var glide = 20
 
 var velocity = Vector2()
 
 func get_input():
-	velocity.x = 0
+	if is_on_floor():
+		on_glide = false
+		velocity.x = 0
 	if Input.is_action_pressed('right'):
 		velocity.x = speed
 	if Input.is_action_pressed('left'):
@@ -33,11 +36,12 @@ func get_input():
 			on_glide = false
 		elif (velocity.y >= 0) and (not on_glide):
 			on_glide = true
+	if Input.is_action_pressed('jump'):
+		if velocity.y < 0:
+			velocity.y += jump_extra
 
 func _physics_process(delta):
 	get_input()
-	if is_on_floor():
-		on_glide = false
 	if on_glide:
 		velocity.y = glide
 	else:
