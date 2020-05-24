de5776d82c15ca16c844494cd63e72d6717c625c
Guido Neulaender <gneulaender@localhost.localdomain>
2020-05-24 19:06:10 -0300

    Enemy update

---
+++ src/RigidBody2D.gd
@@ -0,0 +1,37 @@
+extends RigidBody2D
+
+var target
+var target_vector : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	target_vector = Vector2(0.0,0.0)
+	idle()
+
+func in_range(body, enter):
+	if enter:
+		target = body
+	else:
+		target = null
+
+func see(body):
+	if target != null:
+		var space_state = get_world_2d().direct_space_state
+		var result = space_state.intersect_ray(position, body.position, [self], collision_mask)
+		if body == result.collider:
+			return true
+	return false
+
+func chase(vector : Vector2, delta):
+	pass
+
+func idle():
+	pass
+
+func _physics_process(delta):
+	if see(target):
+		target_vector = (target.position - position).normalized()
+		chase(target_vector, delta)
+	else:
+		target_vector = Vector2(0.0,0.0)
+		idle()
---
+++ src/assets/enemies/EnemyBee.gd
@@ -0,0 +1,7 @@
+extends "res://Enemy.gd"
+
+func chase(vector : Vector2, delta : float):
+	pass
+
+func idle():
+	pass
