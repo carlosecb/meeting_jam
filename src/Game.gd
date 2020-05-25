8d8fb6127e373a19004b23d1b1e641649b129ad8
Carlos E. C. Barbosa <barbosa.carlos.ec@gmail.com>
2020-05-25 19:26:10 -0300

    Change paths to scenes_and_scripts directory

--- src/Game.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _on_Player_player_killed():
-	SceneTransition.change_scene("res://Node2D.tscn")
--- src/scenes_and_scripts/TitleScreen.gd
+++ src/scenes_and_scripts/TitleScreen.gd
@@ -4,4 +4,4 @@ func _ready():
 	$Menu/CenterRow/Buttons/StartGame.grab_focus()
 
 func _on_StartGame_pressed():
-	SceneTransition.change_scene("res://Game.tscn")
+	SceneTransition.change_scene("res://scenes_and_scripts/Game.tscn")
